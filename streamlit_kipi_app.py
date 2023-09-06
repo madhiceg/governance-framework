2f76b32b4fc5d991e1eaff356c31cf440235233a
Madhivanan Anbalagan <31651743+madhiceg@users.noreply.github.com>
2023-09-06 16:54:51 +0530

    4 columns

--- streamlit_kipi_app.py
+++ streamlit_kipi_app.py
@@ -1,3 +1,18 @@
 import streamlit as st
 
 st.title('Welcome')
+
+
+col1, col2, col3, col4 = st.columns([1, 1, 1,1])
+
+with col1:
+  col1.write('Tagged tables')
+
+with col2:
+  col2.write('Tables with a row access policy')
+
+with col3:
+  col3.write('Tagged columns')
+
+with col4:
+  col4.write('Columns with a masking policy')
